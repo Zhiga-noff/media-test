93c78b2f90d5a02253b4cf359e2448ffe096cda8
KZhiganov <KZhiganov@zvezdamedia.ru>
2025-08-05 21:56:24 +0300

    Приступил к созданию инфинит листа

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './assets/styles/index.scss';
 import { routesConstant } from './app/libs/constants';
 import { RouterProvider } from 'react-router-dom';
+import { MainLayout } from "./layout/MainLayout";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <RouterProvider router={routesConstant} />
+    <MainLayout>
+      <RouterProvider router={routesConstant}/>
+    </MainLayout>
   </React.StrictMode>,
 );
---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react';
+import { Breadcrumb, Layout } from "antd";
+import { Content, Header } from "antd/es/layout/layout";
+import styles from './MainLayout.module.scss'
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const MainLayout: FC<Props> = ({ children }) => {
+  return (
+    <Layout>
+      <Header className={styles.header}>
+        <div className={styles.logo}></div>
+      </Header>
+      <Content className={styles.layout}>
+        <Breadcrumb className={styles.breadcrumb}
+                    items={[{ title: 'Главная' }, { title: 'Не основная' }]}
+        />
+        {children}
+      </Content>
+    </Layout>
+  );
+};
